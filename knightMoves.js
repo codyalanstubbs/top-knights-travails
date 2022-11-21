8f9a0cc141234efb851d228f79d376b2011e9138
codyastubbs <5488363+codyastubbs@users.noreply.github.com>
2022-11-21 12:21:07 -0500

    Add driver code to check if all start and end combos paths can be found successfully

--- knightMoves.js
+++ knightMoves.js
@@ -231,3 +231,132 @@ const findPredecessorsMoves = (endMovePosition, predecessorIndex, adjList, moves
     }
     return movesToEnd;
 }
+
+// Driver code to test out all possible combos of start and end positions
+let x = [0,1,2,3,4,5,6,7];
+let y = [0,1,2,3,4,5,6,7];
+
+let combos = combineArrays([x, y] )
+console.log(combos.length)
+let uniqueCombos = combos.filter((c, index) => {
+    return combos.indexOf(c) === index;
+});
+
+let comboCombos = combineArrays([combos, combos]);
+let uniqueComboCombos = comboCombos.filter((c, index) => {
+    return comboCombos.indexOf(c) === index;
+});
+
+let successCounts = 0;
+let failureCounts = 0;
+uniqueComboCombos.forEach((combo) => {
+    const comb1 = [Number(combo.split("")[0]), Number(combo.split("")[1])];
+    const comb2 = [Number(combo.split("")[2]), Number(combo.split("")[3])];
+    (knightMoves(comb1, comb2).length !== 0) ? successCounts++ : failureCounts++;
+
+})
+
+
+console.log(uniqueComboCombos.length)
+console.log(successCounts)
+console.log(failureCounts)
+
+// The functions below come from https://stackoverflow.com/questions/8936610/how-can-i-create-every-combination-possible-for-the-contents-of-two-arrays
+// These are used above to test if all possible starting and ending position combos
+// can be solved by the knightMoves function
+function combineArrays( array_of_arrays ){
+
+    // First, handle some degenerate cases...
+
+    if( ! array_of_arrays ){
+        // Or maybe we should toss an exception...?
+        return [];
+    }
+
+    if( ! Array.isArray( array_of_arrays ) ){
+        // Or maybe we should toss an exception...?
+        return [];
+    }
+
+    if( array_of_arrays.length == 0 ){
+        return [];
+    }
+
+    for( let i = 0 ; i < array_of_arrays.length; i++ ){
+        if( ! Array.isArray(array_of_arrays[i]) || array_of_arrays[i].length == 0 ){
+            // If any of the arrays in array_of_arrays are not arrays or zero-length, return an empty array...
+            return [];
+        }
+    }
+
+    // Done with degenerate cases...
+
+    // Start "odometer" with a 0 for each array in array_of_arrays.
+    let odometer = new Array( array_of_arrays.length );
+    odometer.fill( 0 ); 
+
+    let output = [];
+
+    let newCombination = formCombination( odometer, array_of_arrays );
+
+    output.push( newCombination );
+
+    while ( odometer_increment( odometer, array_of_arrays ) ){
+        newCombination = formCombination( odometer, array_of_arrays );
+        output.push( newCombination );
+    }
+
+    return output;
+}/* combineArrays() */
+
+
+// Translate "odometer" to combinations from array_of_arrays
+function formCombination( odometer, array_of_arrays ){
+    // In Imperative Programmingese (i.e., English):
+    // let s_output = "";
+    // for( let i=0; i < odometer.length; i++ ){
+    //    s_output += "" + array_of_arrays[i][odometer[i]]; 
+    // }
+    // return s_output;
+
+    // In Functional Programmingese (Henny Youngman one-liner):
+    return odometer.reduce(
+      function(accumulator, odometer_value, odometer_index){
+        return "" + accumulator + array_of_arrays[odometer_index][odometer_value];
+      },
+      ""
+    );
+}/* formCombination() */
+
+function odometer_increment( odometer, array_of_arrays ){
+
+    // Basically, work you way from the rightmost digit of the "odometer"...
+    // if you're able to increment without cycling that digit back to zero,
+    // you're all done, otherwise, cycle that digit to zero and go one digit to the
+    // left, and begin again until you're able to increment a digit
+    // without cycling it...simple, huh...?
+
+    for( let i_odometer_digit = odometer.length-1; i_odometer_digit >=0; i_odometer_digit-- ){ 
+
+        let maxee = array_of_arrays[i_odometer_digit].length - 1;         
+
+        if( odometer[i_odometer_digit] + 1 <= maxee ){
+            // increment, and you're done...
+            odometer[i_odometer_digit]++;
+            return true;
+        }
+        else{
+            if( i_odometer_digit - 1 < 0 ){
+                // No more digits left to increment, end of the line...
+                return false;
+            }
+            else{
+                // Can't increment this digit, cycle it to zero and continue
+                // the loop to go over to the next digit...
+                odometer[i_odometer_digit]=0;
+                continue;
+            }
+        }
+    }/* for( let odometer_digit = odometer.length-1; odometer_digit >=0; odometer_digit-- ) */
+
+}/* odometer_increment() */
